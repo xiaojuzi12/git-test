2d1f0bf129739f028bd7c00335e86257105c934c
xiaojuzi <760921420@qq.com>
2021-06-09 15:32:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author xiaojuzi
+ * @create 2021-06-09 15:23
+ */
+public class GitTest {
+    public static void main(String[] args)
+    {
+        System.out.println("hello git!");
+    }
+}
